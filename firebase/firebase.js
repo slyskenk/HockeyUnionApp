50a627d6c36526e8ab42bf8bf057f4d0fd2090e6
Lutalica-source <145153577+Lutalica-source@users.noreply.github.com>
2025-05-31 18:38:55 +0200

    firebase connection

---
+++ firebase/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Z-7mzFScX-BTDD85ShkGGL6iavxeIOE",
+  authDomain: "hockey-union.firebaseapp.com",
+  databaseURL: "https://hockey-union-default-rtdb.firebaseio.com",
+  projectId: "hockey-union",
+  storageBucket: "hockey-union.firebasestorage.app",
+  messagingSenderId: "163997337297",
+  appId: "1:163997337297:web:d3ef5fdb07d0317c86a7f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
